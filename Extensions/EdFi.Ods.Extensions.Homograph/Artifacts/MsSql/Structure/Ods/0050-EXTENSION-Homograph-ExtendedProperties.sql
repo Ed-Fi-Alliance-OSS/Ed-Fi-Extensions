ebb4f9f9c0bf429852ce64802e0c0cc8c7231c4c
semalai <m.semalaiappan@gmail.com>
2021-03-08 11:21:20 -0600

    updated with Structure ODS folder

--- Extensions/EdFi.Ods.Extensions.Homograph/Artifacts/MsSql/Structure/Ods/0050-EXTENSION-Homograph-ExtendedProperties.sql
+++ Extensions/EdFi.Ods.Extensions.Homograph/Artifacts/MsSql/Structure/Ods/0050-EXTENSION-Homograph-ExtendedProperties.sql
@@ -1,8 +1,3 @@
--- SPDX-License-Identifier: Apache-2.0
--- Licensed to the Ed-Fi Alliance under one or more agreements.
--- The Ed-Fi Alliance licenses this file to you under the Apache License, Version 2.0.
--- See the LICENSE and NOTICES files in the project root for more information.
-
 -- Extended Properties [homograph].[Name] --
 EXEC sys.sp_addextendedproperty @name=N'MS_Description', @value=N'The set of elements that comprise a person''s legal name.', @level0type=N'SCHEMA', @level0name=N'homograph', @level1type=N'TABLE', @level1name=N'Name'
 GO
