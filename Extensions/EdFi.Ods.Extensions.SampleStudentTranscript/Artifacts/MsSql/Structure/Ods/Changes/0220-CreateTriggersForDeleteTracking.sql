8f00fb84a8a3307c144234c1cd0f65ae40a74021
Axel Marquez <axel_marqz@hotmail.com>
2022-07-28 15:42:35 -0500

    Apply change queries enhancements to StudentTranscript

--- Extensions/EdFi.Ods.Extensions.SampleStudentTranscript/Artifacts/MsSql/Structure/Ods/Changes/0220-CreateTriggersForDeleteTracking.sql
+++ Extensions/EdFi.Ods.Extensions.SampleStudentTranscript/Artifacts/MsSql/Structure/Ods/Changes/0220-CreateTriggersForDeleteTracking.sql
@@ -1,3 +1,11 @@
+-- SPDX-License-Identifier: Apache-2.0
+-- Licensed to the Ed-Fi Alliance under one or more agreements.
+-- The Ed-Fi Alliance licenses this file to you under the Apache License, Version 2.0.
+-- See the LICENSE and NOTICES files in the project root for more information.
+
+DROP TRIGGER IF EXISTS [samplestudenttranscript].[samplestudenttranscript_InstitutionControlDescriptor_TR_DeleteTracking]
+GO
+
 CREATE TRIGGER [samplestudenttranscript].[samplestudenttranscript_InstitutionControlDescriptor_TR_DeleteTracking] ON [samplestudenttranscript].[InstitutionControlDescriptor] AFTER DELETE AS
 BEGIN
     IF @@rowcount = 0 
@@ -5,8 +13,8 @@ BEGIN
 
     SET NOCOUNT ON
 
-    INSERT INTO [tracked_deletes_samplestudenttranscript].[InstitutionControlDescriptor](InstitutionControlDescriptorId, Id, ChangeVersion)
-    SELECT  d.InstitutionControlDescriptorId, Id, (NEXT VALUE FOR [changes].[ChangeVersionSequence])
+    INSERT INTO [tracked_changes_edfi].[Descriptor](OldDescriptorId, OldCodeValue, OldNamespace, Id, Discriminator, ChangeVersion)
+    SELECT  d.InstitutionControlDescriptorId, b.CodeValue, b.Namespace, b.Id, 'samplestudenttranscript.InstitutionControlDescriptor', (NEXT VALUE FOR [changes].[ChangeVersionSequence])
     FROM    deleted d
             INNER JOIN edfi.Descriptor b ON d.InstitutionControlDescriptorId = b.DescriptorId
 END
@@ -16,6 +24,9 @@ ALTER TABLE [samplestudenttranscript].[InstitutionControlDescriptor] ENABLE TRIG
 GO
 
 
+DROP TRIGGER IF EXISTS [samplestudenttranscript].[samplestudenttranscript_InstitutionLevelDescriptor_TR_DeleteTracking]
+GO
+
 CREATE TRIGGER [samplestudenttranscript].[samplestudenttranscript_InstitutionLevelDescriptor_TR_DeleteTracking] ON [samplestudenttranscript].[InstitutionLevelDescriptor] AFTER DELETE AS
 BEGIN
     IF @@rowcount = 0 
@@ -23,8 +34,8 @@ BEGIN
 
     SET NOCOUNT ON
 
-    INSERT INTO [tracked_deletes_samplestudenttranscript].[InstitutionLevelDescriptor](InstitutionLevelDescriptorId, Id, ChangeVersion)
-    SELECT  d.InstitutionLevelDescriptorId, Id, (NEXT VALUE FOR [changes].[ChangeVersionSequence])
+    INSERT INTO [tracked_changes_edfi].[Descriptor](OldDescriptorId, OldCodeValue, OldNamespace, Id, Discriminator, ChangeVersion)
+    SELECT  d.InstitutionLevelDescriptorId, b.CodeValue, b.Namespace, b.Id, 'samplestudenttranscript.InstitutionLevelDescriptor', (NEXT VALUE FOR [changes].[ChangeVersionSequence])
     FROM    deleted d
             INNER JOIN edfi.Descriptor b ON d.InstitutionLevelDescriptorId = b.DescriptorId
 END
@@ -34,6 +45,9 @@ ALTER TABLE [samplestudenttranscript].[InstitutionLevelDescriptor] ENABLE TRIGGE
 GO
 
 
+DROP TRIGGER IF EXISTS [samplestudenttranscript].[samplestudenttranscript_PostSecondaryOrganization_TR_DeleteTracking]
+GO
+
 CREATE TRIGGER [samplestudenttranscript].[samplestudenttranscript_PostSecondaryOrganization_TR_DeleteTracking] ON [samplestudenttranscript].[PostSecondaryOrganization] AFTER DELETE AS
 BEGIN
     IF @@rowcount = 0 
@@ -41,8 +55,8 @@ BEGIN
 
     SET NOCOUNT ON
 
-    INSERT INTO [tracked_deletes_samplestudenttranscript].[PostSecondaryOrganization](NameOfInstitution, Id, ChangeVersion)
-    SELECT  NameOfInstitution, Id, (NEXT VALUE FOR [changes].[ChangeVersionSequence])
+    INSERT INTO [tracked_changes_samplestudenttranscript].[PostSecondaryOrganization](OldNameOfInstitution, Id, Discriminator, ChangeVersion)
+    SELECT d.NameOfInstitution, d.Id, d.Discriminator, (NEXT VALUE FOR [changes].[ChangeVersionSequence])
     FROM    deleted d
 END
 GO
@@ -51,6 +65,9 @@ ALTER TABLE [samplestudenttranscript].[PostSecondaryOrganization] ENABLE TRIGGER
 GO
 
 
+DROP TRIGGER IF EXISTS [samplestudenttranscript].[samplestudenttranscript_SpecialEducationGraduationStatusDescriptor_TR_DeleteTracking]
+GO
+
 CREATE TRIGGER [samplestudenttranscript].[samplestudenttranscript_SpecialEducationGraduationStatusDescriptor_TR_DeleteTracking] ON [samplestudenttranscript].[SpecialEducationGraduationStatusDescriptor] AFTER DELETE AS
 BEGIN
     IF @@rowcount = 0 
@@ -58,8 +75,8 @@ BEGIN
 
     SET NOCOUNT ON
 
-    INSERT INTO [tracked_deletes_samplestudenttranscript].[SpecialEducationGraduationStatusDescriptor](SpecialEducationGraduationStatusDescriptorId, Id, ChangeVersion)
-    SELECT  d.SpecialEducationGraduationStatusDescriptorId, Id, (NEXT VALUE FOR [changes].[ChangeVersionSequence])
+    INSERT INTO [tracked_changes_edfi].[Descriptor](OldDescriptorId, OldCodeValue, OldNamespace, Id, Discriminator, ChangeVersion)
+    SELECT  d.SpecialEducationGraduationStatusDescriptorId, b.CodeValue, b.Namespace, b.Id, 'samplestudenttranscript.SpecialEducationGraduationStatusDescriptor', (NEXT VALUE FOR [changes].[ChangeVersionSequence])
     FROM    deleted d
             INNER JOIN edfi.Descriptor b ON d.SpecialEducationGraduationStatusDescriptorId = b.DescriptorId
 END
@@ -69,6 +86,9 @@ ALTER TABLE [samplestudenttranscript].[SpecialEducationGraduationStatusDescripto
 GO
 
 
+DROP TRIGGER IF EXISTS [samplestudenttranscript].[samplestudenttranscript_SubmissionCertificationDescriptor_TR_DeleteTracking]
+GO
+
 CREATE TRIGGER [samplestudenttranscript].[samplestudenttranscript_SubmissionCertificationDescriptor_TR_DeleteTracking] ON [samplestudenttranscript].[SubmissionCertificationDescriptor] AFTER DELETE AS
 BEGIN
     IF @@rowcount = 0 
@@ -76,8 +96,8 @@ BEGIN
 
     SET NOCOUNT ON
 
-    INSERT INTO [tracked_deletes_samplestudenttranscript].[SubmissionCertificationDescriptor](SubmissionCertificationDescriptorId, Id, ChangeVersion)
-    SELECT  d.SubmissionCertificationDescriptorId, Id, (NEXT VALUE FOR [changes].[ChangeVersionSequence])
+    INSERT INTO [tracked_changes_edfi].[Descriptor](OldDescriptorId, OldCodeValue, OldNamespace, Id, Discriminator, ChangeVersion)
+    SELECT  d.SubmissionCertificationDescriptorId, b.CodeValue, b.Namespace, b.Id, 'samplestudenttranscript.SubmissionCertificationDescriptor', (NEXT VALUE FOR [changes].[ChangeVersionSequence])
     FROM    deleted d
             INNER JOIN edfi.Descriptor b ON d.SubmissionCertificationDescriptorId = b.DescriptorId
 END
--- Extensions/EdFi.Ods.Extensions.SampleStudentTranscript/Artifacts/PgSql/Structure/Ods/Changes/0040-CreateTriggerUpdateChangeVersionGenerator.sql
+++
@@ -1,3 +0,0 @@
-CREATE TRIGGER UpdateChangeVersion BEFORE UPDATE ON samplestudenttranscript.PostSecondaryOrganization
-    FOR EACH ROW EXECUTE PROCEDURE changes.UpdateChangeVersion();
-
