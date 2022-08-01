9fe27da7a73fee6374b593dd92becf013929a1ee
Axel Marquez <axel_marqz@hotmail.com>
2022-08-01 17:34:41 -0500

    [ODS-5464] Change Queries MetaEd plugin is incorrectly substituting the extension schema in place of the base table's schema in the SQL Server delete triggers (#55)
    * Apply change queries enhancements to StudentTranscript
    
    * Apply change queries enhancements to StudentTransportation
    
    * Apply descriptor schema fix to Sample
    
    * Apply descriptor schema fix to TPDM

--- Extensions/EdFi.Ods.Extensions.SampleStudentTranscript/Artifacts/MsSql/Structure/Ods/Changes/0210-CreateTriggersForChangeVersionAndKeyChanges.sql
+++ Extensions/EdFi.Ods.Extensions.SampleStudentTranscript/Artifacts/MsSql/Structure/Ods/Changes/0210-CreateTriggersForChangeVersionAndKeyChanges.sql
@@ -1,3 +1,11 @@
+-- SPDX-License-Identifier: Apache-2.0
+-- Licensed to the Ed-Fi Alliance under one or more agreements.
+-- The Ed-Fi Alliance licenses this file to you under the Apache License, Version 2.0.
+-- See the LICENSE and NOTICES files in the project root for more information.
+
+DROP TRIGGER IF EXISTS [samplestudenttranscript].[samplestudenttranscript_PostSecondaryOrganization_TR_UpdateChangeVersion]
+GO
+
 CREATE TRIGGER [samplestudenttranscript].[samplestudenttranscript_PostSecondaryOrganization_TR_UpdateChangeVersion] ON [samplestudenttranscript].[PostSecondaryOrganization] AFTER UPDATE AS
 BEGIN
     SET NOCOUNT ON;
--- Extensions/EdFi.Ods.Extensions.SampleStudentTransportation/Artifacts/MsSql/Structure/Ods/Changes/0210-CreateTriggersForChangeVersionAndKeyChanges.sql
+++ Extensions/EdFi.Ods.Extensions.SampleStudentTransportation/Artifacts/MsSql/Structure/Ods/Changes/0210-CreateTriggersForChangeVersionAndKeyChanges.sql
@@ -1,3 +1,11 @@
+-- SPDX-License-Identifier: Apache-2.0
+-- Licensed to the Ed-Fi Alliance under one or more agreements.
+-- The Ed-Fi Alliance licenses this file to you under the Apache License, Version 2.0.
+-- See the LICENSE and NOTICES files in the project root for more information.
+
+DROP TRIGGER IF EXISTS [samplestudenttransportation].[samplestudenttransportation_StudentTransportation_TR_UpdateChangeVersion]
+GO
+
 CREATE TRIGGER [samplestudenttransportation].[samplestudenttransportation_StudentTransportation_TR_UpdateChangeVersion] ON [samplestudenttransportation].[StudentTransportation] AFTER UPDATE AS
 BEGIN
     SET NOCOUNT ON;
