f77bd5b3514897f5e6c2f7366fd8f2086ac961bc
semalai <m.semalaiappan@gmail.com>
2022-10-07 10:53:41 -0500

    function Pack  updated

--- build.githubactions.ps1
+++ build.githubactions.ps1
@@ -131,23 +131,27 @@ function Compile {
 }
 
 function Pack {
-    if ([string]::IsNullOrWhiteSpace($PackageName) -and [string]::IsNullOrWhiteSpace($NuspecFilePath)){
-        Invoke-Execute {
-            dotnet pack $ProjectFile -c $Configuration --output $packageOutput --no-build --verbosity normal -p:VersionPrefix=$version -p:NoWarn=NU5123
-        }
-    }
+    # if ([string]::IsNullOrWhiteSpace($PackageName) -and [string]::IsNullOrWhiteSpace($NuspecFilePath)){
+    #     Invoke-Execute {
+    #         dotnet pack $ProjectFile -c $Configuration --output $packageOutput --no-build --verbosity normal -p:VersionPrefix=$version -p:NoWarn=NU5123
+    #     }
+    # }
     if ($NuspecFilePath -Like "*.nuspec" -and $PackageName -ne $null){
+        Get-Location
+        Write-Host "before nugetRoot "
         $nugetRoot = Split-Path -Path $NuspecFilePath -Parent
         Write-Host "nugetRoot" $nugetRoot
-        Set-Location -Path $nugetRoot
+        Set-Location -Path $nugetRoot -PassThru
+        Write-Host "after nugetRoot "
         Get-Location
+
         nuget pack $NuspecFilePath -OutputDirectory $packageOutput -Version $version -Properties configuration=$Configuration -Properties id=$PackageName -NoPackageAnalysis -NoDefaultExcludes
     }
-    if ([string]::IsNullOrWhiteSpace($NuspecFilePath) -and $PackageName -ne $null){
-        Invoke-Execute {
-            dotnet pack $ProjectFile -c $Configuration --output $packageOutput --no-build --verbosity normal -p:VersionPrefix=$version -p:NoWarn=NU5123 -p:PackageId=$PackageName
-        }
-    }
+    # if ([string]::IsNullOrWhiteSpace($NuspecFilePath) -and $PackageName -ne $null){
+    #     Invoke-Execute {
+    #         dotnet pack $ProjectFile -c $Configuration --output $packageOutput --no-build --verbosity normal -p:VersionPrefix=$version -p:NoWarn=NU5123 -p:PackageId=$PackageName
+    #     }
+    # }
 }
 
 function Publish {
