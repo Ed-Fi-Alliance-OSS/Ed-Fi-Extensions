bbab60f17ff5ceac3674aa28c7d4afd6bb9fde48
Jesus Flores <jesus@simpat.tech>
2022-12-20 18:07:03 -0600

    Rename files with a shorter name

--- Extensions/EdFi.Ods.Extensions.TPDM/Artifacts/MsSql/Data/Security/1030-TPDM-RmvReadChangesFromEdPrepProgClaimSet.sql
+++ Extensions/EdFi.Ods.Extensions.TPDM/Artifacts/MsSql/Data/Security/1030-TPDM-RmvReadChangesFromEdPrepProgClaimSet.sql
@@ -1,4 +1,7 @@
 -- Remove ReadChanges action from all the 'Education Preparation Program' permissions
+
+-- Renamed from 1030-TPDM-Remove-ReadChanges-from-EdPrepProg-Claim-Set.sql to shorten file name.
+
 DECLARE @claimSetId INT, @actionId INT
 
 SELECT @actionId = ActionId FROM Actions WHERE ActionName = 'ReadChanges'
