692cbfe4259cf25b49d26559a917baa4b9f1da5b
m.semalaiappan <m.semalaiappan@gmail.com>
2023-07-20 12:45:06 -0500

    samplestudenttransportation Extension - MetaEd Artifacts updated

--- Extensions/EdFi.Ods.Extensions.SampleStudentTransportation/Versions/1.0.0/Standard/5.0.0/Artifacts/MsSql/Structure/Ods/0030-EXTENSION-SampleStudentTransportation-ForeignKeys.sql
+++ Extensions/EdFi.Ods.Extensions.SampleStudentTransportation/Versions/1.0.0/Standard/5.0.0/Artifacts/MsSql/Structure/Ods/0030-EXTENSION-SampleStudentTransportation-ForeignKeys.sql
@@ -7,15 +7,7 @@ ALTER TABLE [samplestudenttransportation].[StudentTransportation] WITH CHECK ADD
 REFERENCES [edfi].[School] ([SchoolId])
 GO
 
-CREATE NONCLUSTERED INDEX [FK_StudentTransportation_School]
-ON [samplestudenttransportation].[StudentTransportation] ([SchoolId] ASC)
-GO
-
 ALTER TABLE [samplestudenttransportation].[StudentTransportation] WITH CHECK ADD CONSTRAINT [FK_StudentTransportation_Student] FOREIGN KEY ([StudentUSI])
 REFERENCES [edfi].[Student] ([StudentUSI])
-GO
-
-CREATE NONCLUSTERED INDEX [FK_StudentTransportation_Student]
-ON [samplestudenttransportation].[StudentTransportation] ([StudentUSI] ASC)
 GO
 
--- Extensions/EdFi.Ods.Extensions.SampleStudentTransportation/Versions/1.0.0/Standard/5.0.0/Artifacts/PgSql/Structure/Ods/0030-EXTENSION-SampleStudentTransportation-ForeignKeys.sql
+++ Extensions/EdFi.Ods.Extensions.SampleStudentTransportation/Versions/1.0.0/Standard/5.0.0/Artifacts/PgSql/Structure/Ods/0030-EXTENSION-SampleStudentTransportation-ForeignKeys.sql
@@ -7,13 +7,7 @@ ALTER TABLE samplestudenttransportation.StudentTransportation ADD CONSTRAINT FK_
 REFERENCES edfi.School (SchoolId)
 ;
 
-CREATE INDEX FK_68afad_School
-ON samplestudenttransportation.StudentTransportation (SchoolId ASC);
-
 ALTER TABLE samplestudenttransportation.StudentTransportation ADD CONSTRAINT FK_68afad_Student FOREIGN KEY (StudentUSI)
 REFERENCES edfi.Student (StudentUSI)
 ;
-
-CREATE INDEX FK_68afad_Student
-ON samplestudenttransportation.StudentTransportation (StudentUSI ASC);
 
