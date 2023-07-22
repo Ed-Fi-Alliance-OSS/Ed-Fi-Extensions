3c3e4a087d0499c2e39332b85f5bba8b1b094637
Geoffrey McElhanon <geoffrey@mcelhanon.com>
2023-07-22 17:33:53 -0500

    Added license header for all EdOrgId index scripts for TPDM.

--- Extensions/EdFi.Ods.Extensions.TPDM/Versions/1.1.0/Standard/4.0.0/Artifacts/MsSql/Structure/Ods/0060-EXTENSION-TPDM-EdOrgId-Indexes.sql
+++ Extensions/EdFi.Ods.Extensions.TPDM/Versions/1.1.0/Standard/4.0.0/Artifacts/MsSql/Structure/Ods/0060-EXTENSION-TPDM-EdOrgId-Indexes.sql
@@ -1,3 +1,8 @@
+-- SPDX-License-Identifier: Apache-2.0
+-- Licensed to the Ed-Fi Alliance under one or more agreements.
+-- The Ed-Fi Alliance licenses this file to you under the Apache License, Version 2.0.
+-- See the LICENSE and NOTICES files in the project root for more information.
+
 IF NOT EXISTS(SELECT * FROM sys.indexes WHERE name='IX_CandidateEducatorPreparationProgramAssociation_EducationOrganizationId' AND object_id = OBJECT_ID('tpdm.CandidateEducatorPreparationProgramAssociation'))
 BEGIN
     CREATE INDEX IX_CandidateEducatorPreparationProgramAssociation_EducationOrganizationId ON tpdm.CandidateEducatorPreparationProgramAssociation(EducationOrganizationId) INCLUDE (Id)
--- Extensions/EdFi.Ods.Extensions.TPDM/Versions/1.1.0/Standard/4.0.0/Artifacts/PgSql/Structure/Ods/0060-EXTENSION-TPDM-EdOrgId-Indexes.sql
+++ Extensions/EdFi.Ods.Extensions.TPDM/Versions/1.1.0/Standard/4.0.0/Artifacts/PgSql/Structure/Ods/0060-EXTENSION-TPDM-EdOrgId-Indexes.sql
@@ -1,3 +1,8 @@
+-- SPDX-License-Identifier: Apache-2.0
+-- Licensed to the Ed-Fi Alliance under one or more agreements.
+-- The Ed-Fi Alliance licenses this file to you under the Apache License, Version 2.0.
+-- See the LICENSE and NOTICES files in the project root for more information.
+
 CREATE INDEX IF NOT EXISTS IX_daf78c_EducationOrganizationId ON tpdm.CandidateEducatorPreparationProgramAssociation(EducationOrganizationId) INCLUDE (Id);
 CREATE INDEX IF NOT EXISTS IX_edd2a6_EducationOrganizationId ON tpdm.EducatorPreparationProgram(EducationOrganizationId) INCLUDE (Id);
 CREATE INDEX IF NOT EXISTS IX_5cd1ce_EducationOrganizationId ON tpdm.Evaluation(EducationOrganizationId) INCLUDE (Id);
