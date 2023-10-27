da16a28b58c2ee9d90e46f10ae04bbe5d64e1a0a
m.semalaiappan <m.semalaiappan@gmail.com>
2023-10-27 12:10:30 -0500

    EdFi.Ods.Extensions.MyExtension Added

---
+++ Extensions/EdFi.Ods.Extensions.MyExtension/Versions/1.0.0/Standard/5.0.0/Artifacts/MsSql/Data/0001-MyExtension_ResourceClaims.sql
@@ -0,0 +1,15 @@
+﻿
+  
+DECLARE @ParentResourceClaimId INT
+SELECT @ParentResourceClaimId = ResourceClaimId
+FROM [dbo].[ResourceClaims]
+WHERE ResourceName = 'relationshipBasedData'
+ 
+INSERT INTO [dbo].[ResourceClaims] ( [ResourceName]
+                                     ,[ClaimName]     
+                                     ,[ParentResourceClaimId]
+                                     )
+VALUES ('MyExtension'
+        ,'http://ed-fi.org/ods/identity/claims/my-extension/MyExtension'
+        ,@ParentResourceClaimId
+        )
