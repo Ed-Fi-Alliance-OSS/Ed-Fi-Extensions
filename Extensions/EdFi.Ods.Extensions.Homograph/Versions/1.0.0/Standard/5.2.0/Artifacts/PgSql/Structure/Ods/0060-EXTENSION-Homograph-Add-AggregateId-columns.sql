cb1b7965d6776a3b5f3082764b901188eef29d72
m.semalaiappan <m.semalaiappan@gmail.com>
2024-09-30 13:14:43 -0500

    Script Updated

--- Extensions/EdFi.Ods.Extensions.Homograph/Versions/1.0.0/Standard/5.2.0/Artifacts/PgSql/Structure/Ods/0060-EXTENSION-Homograph-Add-AggregateId-columns.sql
+++ Extensions/EdFi.Ods.Extensions.Homograph/Versions/1.0.0/Standard/5.2.0/Artifacts/PgSql/Structure/Ods/0060-EXTENSION-Homograph-Add-AggregateId-columns.sql
@@ -6,16 +6,16 @@
 -- Create sequences, alter tables to add the AggregateId column, and create indexes in PostgreSQL
 
 -- For the "Contact" table
-CREATE SEQUENCE homograph.contact_aggseq START WITH -2147483648 INCREMENT BY 1;
+CREATE SEQUENCE homograph.contact_aggseq START WITH -2147483648 INCREMENT BY 1 MINVALUE -2147483648;
 ALTER TABLE homograph.contact ADD COLUMN aggregateid INT NOT NULL DEFAULT nextval('homograph.contact_aggseq');
 CREATE INDEX ix_contact_aggregateid ON homograph.contact (aggregateid);
 
 -- For the "Name" table
-CREATE SEQUENCE homograph.name_aggseq START WITH -2147483648 INCREMENT BY 1;
+CREATE SEQUENCE homograph.name_aggseq START WITH -2147483648 INCREMENT BY 1 MINVALUE -2147483648;
 ALTER TABLE homograph.name ADD COLUMN aggregateid INT NOT NULL DEFAULT nextval('homograph.name_aggseq');
 CREATE INDEX ix_name_aggregateid ON homograph.name (aggregateid);
 
 -- For the "School" table
-CREATE SEQUENCE homograph.school_aggseq START WITH -2147483648 INCREMENT BY 1;
+CREATE SEQUENCE homograph.school_aggseq START WITH -2147483648 INCREMENT BY 1 MINVALUE -2147483648;
 ALTER TABLE homograph.school ADD COLUMN aggregateid INT NOT NULL DEFAULT nextval('homograph.school_aggseq');
 CREATE INDEX ix_school_aggregateid ON homograph.school (aggregateid);
--- Extensions/EdFi.Ods.Extensions.Sample/Versions/1.0.0/Standard/5.2.0/Artifacts/PgSql/Structure/Ods/0060-EXTENSION-Sample-Add-AggregateId-columns.sql
+++ Extensions/EdFi.Ods.Extensions.Sample/Versions/1.0.0/Standard/5.2.0/Artifacts/PgSql/Structure/Ods/0060-EXTENSION-Sample-Add-AggregateId-columns.sql
@@ -4,16 +4,16 @@
 -- See the LICENSE and NOTICES files in the project root for more information.
 
 -- For the "Bus" table
-CREATE SEQUENCE sample.bus_aggseq START WITH -2147483648 INCREMENT BY 1;
+CREATE SEQUENCE sample.bus_aggseq START WITH -2147483648 INCREMENT BY 1 MINVALUE -2147483648;
 ALTER TABLE sample.bus ADD COLUMN aggregateid INT NOT NULL DEFAULT nextval('sample.bus_aggseq');
 CREATE INDEX ix_bus_aggregateid ON sample.bus (aggregateid);
 
 -- For the "BusRoute" table
-CREATE SEQUENCE sample.busroute_aggseq START WITH -2147483648 INCREMENT BY 1;
+CREATE SEQUENCE sample.busroute_aggseq START WITH -2147483648 INCREMENT BY 1 MINVALUE -2147483648;
 ALTER TABLE sample.busroute ADD COLUMN aggregateid INT NOT NULL DEFAULT nextval('sample.busroute_aggseq');
 CREATE INDEX ix_busroute_aggregateid ON sample.busroute (aggregateid);
 
 -- For the "StudentGraduationPlanAssociation" table
-CREATE SEQUENCE sample.studentgraduationplanassociation_aggseq START WITH -2147483648 INCREMENT BY 1;
+CREATE SEQUENCE sample.studentgraduationplanassociation_aggseq START WITH -2147483648 INCREMENT BY 1 MINVALUE -2147483648;
 ALTER TABLE sample.studentgraduationplanassociation ADD COLUMN aggregateid INT NOT NULL DEFAULT nextval('sample.studentgraduationplanassociation_aggseq');
 CREATE INDEX ix_studentgraduationplanassociation_aggregateid ON sample.studentgraduationplanassociation (aggregateid);
