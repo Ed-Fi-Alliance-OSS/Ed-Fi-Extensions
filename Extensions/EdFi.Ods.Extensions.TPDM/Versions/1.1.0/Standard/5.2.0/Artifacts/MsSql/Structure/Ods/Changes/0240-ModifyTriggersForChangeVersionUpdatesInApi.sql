758dc19a1c41e6b466c24a2015f795106345ad97
Geoffrey McElhanon <geoffrey@mcelhanon.com>
2025-02-09 17:06:28 -0600

    SQL Server TPDM scripts.

--- Extensions/EdFi.Ods.Extensions.TPDM/Versions/1.1.0/Standard/5.2.0/Artifacts/MsSql/Structure/Ods/Changes/0240-ModifyTriggersForChangeVersionUpdatesInApi.sql
+++ Extensions/EdFi.Ods.Extensions.TPDM/Versions/1.1.0/Standard/5.2.0/Artifacts/MsSql/Structure/Ods/Changes/0240-ModifyTriggersForChangeVersionUpdatesInApi.sql
@@ -30,6 +30,17 @@ GO
 DROP TRIGGER IF EXISTS [tpdm].[tpdm_EvaluationRating_TR_UpdateChangeVersion]
 GO
 
+CREATE TRIGGER [tpdm].[tpdm_EvaluationRating_TR_UpdateChangeVersion] ON [tpdm].[EvaluationRating] AFTER UPDATE AS
+BEGIN
+    SET NOCOUNT ON;
+    -- Handle cascading key changes
+    UPDATE [tpdm].[EvaluationRating]
+    SET ChangeVersion = (NEXT VALUE FOR [changes].[ChangeVersionSequence])
+    FROM [tpdm].[EvaluationRating] u
+    WHERE EXISTS (SELECT 1 FROM inserted i WHERE i.AggregateId = u.AggregateId AND i.ChangeVersion = u.ChangeVersion);
+END	
+GO
+
 DROP TRIGGER IF EXISTS [tpdm].[tpdm_FinancialAid_TR_UpdateChangeVersion]
 GO
 
@@ -47,3 +58,4 @@ GO
 
 DROP TRIGGER IF EXISTS [tpdm].[tpdm_SurveySectionResponsePersonTargetAssociation_TR_UpdateChangeVersion]
 GO
+
