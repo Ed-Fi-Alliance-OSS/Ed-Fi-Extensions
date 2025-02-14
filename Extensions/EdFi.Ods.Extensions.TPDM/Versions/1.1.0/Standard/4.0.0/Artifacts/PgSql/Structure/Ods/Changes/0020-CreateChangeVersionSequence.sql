4ba0fa0eecfda1104aca2a95548afc0bd2845d6f
Geoffrey McElhanon <geoffrey@mcelhanon.com>
2025-02-14 00:13:27 -0600

    Modifying the 0020-CreateChangeVersionSequence scripts for extensions so that they don't stomp on the new version of the UpdateChangeVersion function that is executed with the standard's changes scripts.

--- Extensions/EdFi.Ods.Extensions.TPDM/Versions/1.1.0/Standard/4.0.0/Artifacts/PgSql/Structure/Ods/Changes/0020-CreateChangeVersionSequence.sql
+++ Extensions/EdFi.Ods.Extensions.TPDM/Versions/1.1.0/Standard/4.0.0/Artifacts/PgSql/Structure/Ods/Changes/0020-CreateChangeVersionSequence.sql
@@ -5,7 +5,7 @@
 
 CREATE SEQUENCE IF NOT EXISTS changes.ChangeVersionSequence START WITH 1;
 
-CREATE OR REPLACE FUNCTION changes.updateChangeVersion()
+CREATE FUNCTION IF NOT EXISTS changes.updateChangeVersion()
     RETURNS trigger AS
 $BODY$
 BEGIN
